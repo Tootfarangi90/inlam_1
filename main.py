416dc2fa2213e0053dabf3f824324bf332e47b91
Ayda Sholani <shoolani@gmail.com>
2023-09-27 17:09:42 +0200

    main.py refactored

--- main.py
+++ main.py
@@ -1,22 +1,49 @@
-from my_modules.rovarspraket_function import Rovarspraket
-from my_modules.fetch_from_github import FetchFileFromGitHub
-
-github_link = "https://github.com/AIDEV23S/svText"
-file_name = "svenskt_text.txt"
-imported_text_content = FetchFileFromGitHub(github_link, file_name)
-
-rovarspraket = Rovarspraket(imported_text_content)
+from my_modules.ask_user import AskUserYesOrNo
+from my_modules.encrypted_email_handler import EncryptedEmailHandler
+from my_modules.github_file_manager import GitHubFileManager
+from my_modules.local_file_manager import FileManager
 
 
 def main():
     try:
-        invalid_chars = rovarspraket.find_invalid_characters()
+        # Skapa en instans av FileManager för filhantering
+        file_manager = FileManager()
+
+        # Skapa en instans av GitHubFileManager för att hämta fil från GitHub
+        github_file_manager = GitHubFileManager()
+
+        # Hämta filinnehåll från GitHub
+        github_file_content = github_file_manager.fetch_github_file()
+
+        # Skapa en instans av EncryptedEmailHandler för att hantera textöversättning
+        translator = EncryptedEmailHandler(github_file_content)
+
+        # Hitta och skriv ut ogiltiga tecken i den hämtade texten
+        invalid_chars = translator.find_and_print_invalid_characters()
+
         if invalid_chars:
-            print(rovarspraket.print_invalid_characters())
+            print(invalid_chars)
         else:
-            translated_text = rovarspraket.translate_to_rovarspraket()
-            print("Översatt text:")
+            # Översätta texten till rövarspråket
+            translated_text = translator.translate_to_rovarspraket()
             print(translated_text)
+
+            # Fråga om användaren vill spara översättningen i den här mappen
+            translated_file_path = file_manager.input_local_file_path(
+                "Vill du spara översättningen i den här mappen? "
+            )
+
+            # Skriv den översatta texten till filen (skapar filen om den inte existerar)
+            file_manager.write_file(translated_file_path, translated_text)
+
+            # Fråga om användaren vill läsa den översatta texten direkt i terminalen
+            ask_to_read = AskUserYesOrNo(
+                "Vill du läsa den översatta texten direkt i terminalen?"
+            )
+
+            # Om ja, skriv ut texten från filen
+            if ask_to_read:
+                print(file_manager.read_file(translated_file_path))
 
     except Exception as e:
         print(f"Exception: {e}")
