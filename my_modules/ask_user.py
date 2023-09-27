4a88579f7aceb81df21f90585f6c63bac17ba243
Ayda Sholani <shoolani@gmail.com>
2023-09-27 14:03:27 +0200

    separated ask user yes or no function

---
+++ my_modules/ask_user.py
@@ -0,0 +1,17 @@
+import os
+
+
+def YesOrNo(prompt_message):
+    while True:
+        try:
+            ask_user = input(prompt_message).strip().lower()[0]
+            if ask_user in ["y", "j"]:
+                return True
+            elif ask_user in ["n"]:
+                return False
+            else:
+                print("Ogiltigt svar, svara med ja eller nej")
+
+        except KeyboardInterrupt:
+            print("Programmet avslutas!")
+            exit()
