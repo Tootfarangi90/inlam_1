1f8a93f42cd26c307d1a452695c6f0fd585984f9
Ayda Sholani <shoolani@gmail.com>
2023-09-27 17:14:40 +0200

    refactor github file managment class

--- my_modules/github_file_manager.py
+++ my_modules/github_file_manager.py
@@ -1,28 +1,36 @@
 import requests
-from .ask_user import YesOrNo
+from .ask_user import AskUserYesOrNo
 
 
-def FetchGitHubFile():
-    try:
-        full_path = ""
-        if YesOrNo("Vill du ange en egen GitHub-länk? "):
-            name_and_repo = input("Ange användarnamn och repository (username/repo): ")
-            file_path = input("Ange filnamn: ")
+class GitHubFileManager:
+    def __init__(self):
+        self.full_path = ""
 
-            if name_and_repo.strip() and file_path.strip():
-                full_path += "https://github.com/{}/raw/main/{}".format(
-                    name_and_repo, file_path
+    def fetch_github_file(self):
+        try:
+            if AskUserYesOrNo("Vill du ange en egen GitHub-länk? "):
+                name_and_repo = input(
+                    "Ange användarnamn och repository (username/repo): "
                 )
-        else:
-            full_path += "https://github.com/AIDEV23S/svText/raw/main/svenskt_text.txt"
+                file_path = input("Ange filnamn: ")
 
-        res = requests.get(full_path)
+                if name_and_repo.strip() and file_path.strip():
+                    self.full_path += "https://github.com/{}/raw/main/{}".format(
+                        name_and_repo, file_path
+                    )
+            else:
+                self.full_path += (
+                    "https://github.com/AIDEV23S/svText/raw/main/svenskt_text.txt"
+                )
 
-        if res.status_code == 200:
-            file_content = res.text
-            return file_content
-        else:
-            print(f"Det gick inte att hämta filen. Statuskod: {res.status_code}")
-            return None
-    except Exception as e:
-        print(f"Ett fel inträffade: {str(e)}")
+            res = requests.get(self.full_path)
+
+            if res.status_code == 200:
+                file_content = res.text
+                print(file_content)
+                return file_content
+            else:
+                print(f"Det gick inte att hämta filen. Statuskod: {res.status_code}")
+                return None
+        except Exception as e:
+            print(f"Ett fel inträffade: {str(e)}")
