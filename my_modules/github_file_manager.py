4ee0c17016e2c8812cb04dd933d0a087bc7d6732
Ayda Sholani <shoolani@gmail.com>
2023-09-29 11:27:10 +0200

    refactor and added comments

--- my_modules/github_file_manager.py
+++ my_modules/github_file_manager.py
@@ -2,12 +2,16 @@ import requests
 from .ask_user import AskUserYesOrNo
 
 
+# En class för att hämta och hantera filer från GitHub
 class GitHubFileManager:
     def __init__(self):
-        self.full_path = ""
+        # Default värde om användare inte anger något
+        self.full_path = "https://github.com/AIDEV23S/svText/raw/main/svenskt_text.txt"
 
+    # Hämta en textfil från github
     def fetch_github_file(self):
         try:
+            # Frågar användaren om de vill ange egen GitHub-länk
             if AskUserYesOrNo("Vill du ange en egen GitHub-länk? "):
                 name_and_repo = input(
                     "Ange användarnamn och repository (username/repo): "
@@ -15,21 +19,20 @@ class GitHubFileManager:
                 file_path = input("Ange filnamn: ")
 
                 if name_and_repo.strip() and file_path.strip():
-                    self.full_path += "https://github.com/{}/raw/main/{}".format(
+                    # Skapar den fullständiga GitHub-url med användarens inmatning
+                    self.full_path = "https://github.com/{}/raw/main/{}".format(
                         name_and_repo, file_path
                     )
-            else:
-                self.full_path += (
-                    "https://github.com/AIDEV23S/svText/raw/main/svenskt_text.txt"
-                )
-
+            # GET-request till GitHub-url
             res = requests.get(self.full_path)
-
+            # Kontrollera HTTP-statuskod 200 dvs om det är ok
             if res.status_code == 200:
                 file_content = res.text
                 return file_content
             else:
+                # Om det inte går att hämta filen returneras ett felmeddelande med statuskod
                 print(f"Det gick inte att hämta filen. Statuskod: {res.status_code}")
                 return None
         except Exception as e:
+            # Andra fel som kan uppstå
             print(f"Ett fel inträffade: {str(e)}")
