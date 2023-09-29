f71b3a3e0323a5c0291301bc2fa31121552125a8
Ayda Sholani <shoolani@gmail.com>
2023-09-29 10:57:11 +0200

    Added comments on ask_user.py

--- my_modules/ask_user.py
+++ my_modules/ask_user.py
@@ -1,17 +1,23 @@
 import os
 
 
+# Funktion som tar in en prompt där man kan fråga användaren ja och nej frågor
 def AskUserYesOrNo(prompt_message):
     while True:
         try:
+            # Läser inmatning från användare och tar bort onödiga white spaces, gör om till små bokstäver och tar ut första tecknet
             ask_user = input(prompt_message).strip().lower()[0]
+            # Om användaren svara 'ja(j)' eller yes(y)' returnerar funktionen True
             if ask_user in ["y", "j", "y"]:
                 return True
+            # Om användaren svarar 'nej(n)' eller 'no(n)' returneras False
             elif ask_user in ["n"]:
                 return False
             else:
+                # Om användaren svarar med något annat returneras detta meddelande
                 print("Ogiltigt svar, svara med ja eller nej")
 
         except KeyboardInterrupt:
+            # Om användare avbryter programmet med Ctrl+C
             print("Programmet avslutas!")
             exit()
